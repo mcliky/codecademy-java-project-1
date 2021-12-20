a54391552237b872235d60e6b46102f8555023a2
Thomas Mclik <tmaszuraev@gmail.com>
2021-12-20 16:07:49 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+// Define your tree class in this file!
+public class Main{
+    public static void main(String[] args){
+        //We’re going to introduce ourselves and print a tree to the screen.
+        System.out.println("Hello my name is Thomas");
+        System.out.println("I will plant a tree right now");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+        System.out.println("--------");
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+
+        System.out.println("   *");
+        System.out.println("   *");
+
+
+    }
+}
